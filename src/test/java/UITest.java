cae5b1b5b0d43ba0bfdb00df24988c5c8c863b9f
Lizamin <elyzavetaminakova@gmail.com>
2021-08-09 21:20:22 +0300

    new tests update

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -7,10 +7,13 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.URI;
+import java.net.URL;
+
 public class UITest {
 
     WebDriver driver;
-    private final String baseUrl = "https://www.ikea.com/ua/uk/temp";
+    private final String baseUrl = "https://www.ikea.com/ua/uk/";
 
     @BeforeClass
     public void setUpDriver() {
@@ -39,6 +42,26 @@ public class UITest {
         String currentUrl = driver.getCurrentUrl();
         System.out.println(currentUrl);
         Assert.assertEquals(currentUrl, "https://www.ikea.com/ua/uk/", "Error is not as expected");
+    }
+
+    @Test
+    public void myUITest3() {
+        driver.get(baseUrl);
+        String currentTitle = driver.getTitle();
+        System.out.println(currentTitle);
+    //    Assert.assertEquals(currentTitle, "404 - Ой! Щось пішло не так :( - IKEA", "404 - Ой! Щось пішло не так :( - IKEA");
+        Assert.assertTrue(currentTitle.contains("IKEA"));
+    }
+
+    @Test
+    public void myUITest4() {
+        driver.navigate().to("https://www.ikea.com/ua/uk/");
+        driver.navigate().to("https://www.ikea.com/ua/uk/temp");
+        driver.navigate().back();
+        driver.navigate().refresh();
+        String currentTitle = driver.getTitle();
+        System.out.println(currentTitle);
+        Assert.assertTrue(currentTitle.contains("IKEA"));
     }
 
 }
