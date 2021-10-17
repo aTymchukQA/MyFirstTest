0f7b1c43917d7e3d53f8d6940076a2d6e6920f7e
aTymchukQA <a.tymchuk@twinwingames.com>
2021-10-17 20:46:26 +0300

    screenshots

--- src/test/java/LocalizationScreenshots.java
+++ src/test/java/LocalizationScreenshots.java
@@ -1,27 +1,34 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class LocalizationScreenshots {
 
     @Test
-    public void makeScreenshots() {
+    public void makeScreenshots() throws IOException {
         WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("http://dev-cl.twinwingames.com:85/relax/onewaybook/test/build-107/?moneymode=fun&cheatsOn=true&developerMode=true");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement preLoader = driver.findElement(By.id("preLoader"));
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         WebElement start = driver.findElement(By.xpath("//span[contains(text(), 'START')]"));
-        /*if (start.isDisplayed()) {
-            new AShot()
-                    .shootingStrategy(ShootingStrategies.viewportPasting(100))
-                    .takeScreenshot((WebDriver) preLoader);
-        }*/
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        File source_file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source_file, new File("D:\\localizationAutomated\\loadingScreen.png"));
+        start.click();
+        WebElement spin = driver.findElement(By.className("spin-button"));
+        if (spin.isDisplayed()) {File source_file2 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source_file2, new File("D:\\localizationAutomated\\mainGame.png"));}
+        WebElement settings = driver.findElement(By.className("sprite"));
+        settings.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        File source_file3 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source_file3, new File("D:\\localizationAutomated\\paytable.png"));
     }
 }
