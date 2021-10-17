12e8f25ee9a5b04f292a5f1c4f6f7363aa0b5621
aTymchukQA <a.tymchuk@twinwingames.com>
2021-10-17 15:40:09 +0300

    executeScript to click START

--- src/test/java/GameDev.java
+++ src/test/java/GameDev.java
@@ -1,10 +1,12 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -12,16 +14,15 @@ import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class GameDev {
 
-    WebDriver driver;
-
     @Test
     public void gameRulesText() {
+        WebDriver driver;
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         JavascriptExecutor js = (JavascriptExecutor)driver;
         driver.get("http://dev.twinwingames.com:85/relax/leprechaun/test/build-180/?ticket=testticket-twg33&cheatsOn=true&developerMode=true");
         //$(By.xpath("//span[contains(text(), 'START')]")).click();
-        WebElement start = driver.findElement(By.className("block-with-skip-button"));
+        WebElement start = driver.findElement(By.xpath("//span[contains(text(), 'START')]"));
         js.executeScript("arguments[0].click();", start);
         //executeJavaScript("arguments[0].click()", start);
         //$(By.className("buttonwrapper sprite")).click();
